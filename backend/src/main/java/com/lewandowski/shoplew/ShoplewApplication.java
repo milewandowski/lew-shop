b4d86098ff1f2bc2473c3acc4047249f64458ce1
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-02-23 16:06:35 +0000

    Initial commit

---
+++ backend/src/main/java/com/lewandowski/shoplew/ShoplewApplication.java
@@ -0,0 +1,13 @@
+package com.lewandowski.shoplew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoplewApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/lewandowski/shoplew/ShoplewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewandowski.shoplew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
