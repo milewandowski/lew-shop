e83c8eb13b916f5e2179df7194d94d3a95d8adee
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-02-24 11:47:15 +0100

    Add cross origin support for rest api

--- backend/src/main/java/com/lewandowski/shoplew/repository/CategoryRepository.java
+++ backend/src/main/java/com/lewandowski/shoplew/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.lewandowski.shoplew.repository;
 import com.lewandowski.shoplew.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "category", path = "category")
+@CrossOrigin("http://localhost:4200")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
