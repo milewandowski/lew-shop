a8ac24b27253929e0750653e7b7d3c79033b4183
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-03-05 19:53:57 +0100

    Change cart-status component to subscribe data from service

--- frontend/src/app/components/cart-status/cart-status.component.ts
+++ frontend/src/app/components/cart-status/cart-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartStatusComponent implements OnInit {
 
-  constructor() { }
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
   }
 
 }
