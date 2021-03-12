8fc6a7577422951fc865724a8b466a962da937b0
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-03-12 12:07:22 +0100

    Create notOnlyWhitespace validator

---
+++ frontend/src/app/validators/lewshop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { LewshopValidators } from './lewshop-validators';
+
+describe('LewshopValidators', () => {
+  it('should create an instance', () => {
+    expect(new LewshopValidators()).toBeTruthy();
+  });
+});
