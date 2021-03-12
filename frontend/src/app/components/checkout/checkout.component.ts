68c241e8e595926c25b14fb3dee1d62f58fba059
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-03-12 12:22:38 +0100

    Create getters for form fields and add notOnlyWhiteSpace validator

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { CheckoutFormService } from 'src/app/services/checkout-form.service';
+import { LewshopValidators } from 'src/app/validators/lewshop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -32,22 +33,22 @@ export class CheckoutComponent implements OnInit {
   createCheckoutFormGroup() {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required]),
-        lastName: new FormControl('', [Validators.required]),
+        firstName: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
+        lastName: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
         email: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,4}$')]),
         phoneNumber: new FormControl('', [Validators.required,
           Validators.pattern('(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s\.]?[(]?[0-9]{1,3}[)]?([-\s\.]?[0-9]{3})([-\s\.]?[0-9]{3,4})')])
       }),
       shippingAddress: this.formBuilder.group({
-        street: new FormControl('', [Validators.required]),
-        city: new FormControl('', [Validators.required]),
-        zipCode: new FormControl('', [Validators.required]),
+        street: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
+        city: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
+        zipCode: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
         country: new FormControl('', [Validators.required])
       }),
       billingAddress: this.formBuilder.group({
-        street: new FormControl('', [Validators.required]),
-        city: new FormControl('', [Validators.required]),
-        zipCode: new FormControl('', [Validators.required]),
+        street: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
+        city: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
+        zipCode: new FormControl('', [Validators.required, LewshopValidators.notOnlyWhitespace]),
         country: new FormControl('', [Validators.required])
       })
     });
@@ -66,5 +67,20 @@ export class CheckoutComponent implements OnInit {
     //TODO: Handle submit button click
     console.log(this.checkoutFormGroup.get('customer').value);
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+  get phoneNumber() { return this.checkoutFormGroup.get('customer.phoneNumber'); }
+
+  get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
+  get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
+  get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+  get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
+
+  get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street'); }
+  get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city'); }
+  get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode'); }
+  get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country'); }
 
 }
