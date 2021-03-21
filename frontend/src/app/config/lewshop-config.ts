2dd5f7e477371f1c0d7599bade298da064d787c4
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-03-21 19:48:16 +0100

    Create lewshop-config for okta configuration

---
+++ frontend/src/app/config/lewshop-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oaceh8b7TDSeclFg5d6',
+        issuer: 'https://dev-95542939.okta.com/oauth2/default',
+        redirectUri: 'http://localhost4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
